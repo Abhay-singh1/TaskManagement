a4e9cea037c645558c1c2f8c1d43f58bb48eacb6
Abhay Singh <54500897+Abhay-singh1@users.noreply.github.com>
2023-07-15 17:53:33 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
     const handleSubmit = async(e)=>{
         e.preventDefault()
     try {
-        const response = await axios.post('http://localhost:5000/auth/login',{
+        const response = await axios.post('https://task-management-rouge.vercel.app//auth/login',{
             email,
             password
         })
